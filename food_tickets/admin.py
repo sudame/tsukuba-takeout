78b968e724d60edba6e24308f385ee48eabff161
Mikihiro SUDA <su.glasses.923@gmail.com>
2020-04-28 23:21:25 +0900

    update: モデルを本番に近づける

--- food_tickets/admin.py
+++ food_tickets/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Store, Ticket
+from .models import (
+    Store,
+    Ticket,
+    StoreArea,
+    StoreGenre,
+    StoreOpeningHour,
+    StoreTicketHour,
+)
 
 
 class TicketInline(admin.StackedInline):
@@ -7,8 +14,20 @@ class TicketInline(admin.StackedInline):
     extra = 3
 
 
+class TicketHourInline(admin.StackedInline):
+    model = StoreTicketHour
+    extra = 1
+
+
+class OpeningHourInline(admin.StackedInline):
+    model = StoreOpeningHour
+    extra = 1
+
+
 class StoreAdmin(admin.ModelAdmin):
-    inlines = [TicketInline]
+    inlines = [TicketInline, OpeningHourInline, TicketHourInline]
 
 
 admin.site.register(Store, StoreAdmin)
+admin.site.register(StoreArea)
+admin.site.register(StoreGenre)
--- food_tickets/models.py
+++ food_tickets/models.py
@@ -1,19 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class StoreGenre(models.Model):
+    name = models.TextField(unique=True)
+    slug = models.CharField(unique=True, max_length=24)
+
+    def __str__(self):
+        return self.name
+
+
+class StoreArea(models.Model):
+    name = models.TextField(unique=True)
+    slug = models.CharField(unique=True, max_length=24)
+
+    def __str__(self):
+        return self.name
 
 
 class Store(models.Model):
-    name = models.CharField(max_length=300)
+    name = models.TextField(unique=True)
+    slug = models.CharField(unique=True, max_length=24)
+    genres = models.ManyToManyField(to=StoreGenre)
+    address = models.TextField()
+    area = models.ForeignKey(to=StoreArea, on_delete=models.CASCADE)
+    short_description = models.CharField(max_length=300)
+    description = models.TextField()
+    notice = models.TextField()
 
     def __str__(self):
         return self.name
 
 
 class Ticket(models.Model):
-    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name="tickets")
+    store = models.ForeignKey(
+        to=Store, on_delete=models.CASCADE, related_name="tickets"
+    )
     name = models.CharField(max_length=300)
+    expire = models.DateField(blank=True, null=True)
     price = models.IntegerField()
 
     def __str__(self):
         return self.name
+
+
+class StoreOpeningHour(models.Model):
+    store = models.ForeignKey(
+        to=Store, on_delete=models.CASCADE, related_name="opening_hours"
+    )
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+
+class StoreTicketHour(models.Model):
+    store = models.ForeignKey(
+        to=Store, on_delete=models.CASCADE, related_name="ticket_hours"
+    )
+    start_time = models.TimeField()
+    end_time = models.TimeField()
