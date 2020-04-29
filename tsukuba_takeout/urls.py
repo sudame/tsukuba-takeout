ada20ae53972aa085d6e03fbe900298415cc4682
Mikihiro SUDA <su.glasses.923@gmail.com>
2020-04-29 23:27:56 +0900

    add: api

--- tsukuba_takeout/urls.py
+++ tsukuba_takeout/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("", include("food_tickets.urls")),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
+    path("api/", include("api.urls")),
 ]
