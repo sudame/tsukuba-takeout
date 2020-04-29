8c9c0e587940f57dd84a3ba2675cde367354001a
Mikihiro SUDA <su.glasses.923@gmail.com>
2020-04-29 23:27:48 +0900

    add: install django rest framework

--- tsukuba_takeout/settings.py
+++ tsukuba_takeout/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
+    "rest_framework",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
