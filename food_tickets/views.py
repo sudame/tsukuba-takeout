f7ef5b8beac9b47e62f43d691b3886c3461057c2
Mikihiro SUDA <su.glasses.923@gmail.com>
2020-04-30 18:53:30 +0900

    add: very simple error handling

--- food_tickets/views.py
+++ food_tickets/views.py
@@ -7,7 +7,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from api.serializers import StoreSerializer
 import json
 import payjp
+from payjp.error import PayjpException
 import os
+from .forms import PaymentForm
 
 payjp.api_key = os.getenv("PAYJP_SECRET_KEY")
 
@@ -17,14 +19,14 @@ class StoreListView(generic.ListView):
     template_name = "food_tickets/storeList.html"
 
     def get_context_data(self, **kwargs):
-        data = super().get_context_data(kwargs)
+        data = super().get_context_data()
         data["store_list_object"] = json.dumps(
             StoreSerializer(Store.objects.all(), many=True).data, ensure_ascii=False
         )
         return data
 
 
-class StoreView(LoginRequiredMixin, generic.DetailView):
+class StoreView(generic.DetailView):
     model = Store
     template_name = "food_tickets/store.html"
 
@@ -38,11 +40,24 @@ class TicketDetailView(generic.DetailView):
     model = Ticket
 
 
-class TicketBuyView(generic.DetailView, LoginRequiredMixin):
+class TicketBuyView(LoginRequiredMixin, generic.DetailView):
     model = Ticket
     template_name = "food_tickets/ticket_buy.html"
 
+    # TODO: NEED refactoring!!!!!!!!!!!!!!!!!
     def post(self, request: HttpRequest, *args, **kwargs):
-        payjp_token = request.POST["payjp-token"]
-        charge = payjp.Charge.create(amount=300, card=payjp_token, currency="jpy",)
-        return HttpResponse(json.dumps(charge), content_type="application/json")
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            self.object = self.get_object()
+            ticket: Ticket = super(TicketBuyView, self).get_context_data()["ticket"]
+            try:
+                charge = payjp.Charge.create(
+                    amount=ticket.price,
+                    card=form.cleaned_data["payjp_token"],
+                    currency="jpy",
+                )
+            except PayjpException as payjp_err:
+                return HttpResponse("error: {0}".format(payjp_err))
+            return HttpResponse(json.dumps(charge), content_type="application/json")
+        else:
+            return HttpResponse("error.")
